af6f3bb1546d4645ecff58e37efe37af6b99315a
ilkproje <emreebegil145@gmail.com>
2025-12-12 10:27:59 +0300

    ilk proje

---
+++ app/src/main/java/com/example/a24580222ilkproje/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.textView.text = "Merhaba Emre"
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Kaydedildi "
+        }
+        binding.button2.setOnClickListener {
+            binding.textView.text =" iptal edildi"
+        }
+
+    }
+
+}
